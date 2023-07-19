9ba0216cc6f6df49e20e5a60631c4238198ba831
Yvan Yang <ybyang@best-inc.com>
2023-07-19 09:40:16 +0800

    astro docs

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 const puppeteer = require("puppeteer");
-const cheerio = require("cheerio");
 const fs = require("fs");
 const async = require("async");
 const PDFLib = require("pdf-lib");
 const PDFDocument = PDFLib.PDFDocument;
-const path = require("path");
 
 // Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36
 const userAgent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko)";
 
-const part = "vue3-guide";
-const ROOTURL = `https://cn.vuejs.org/guide/`;
-const rootURL = `https://cn.vuejs.org/guide/introduction.html`;
+const part = "astro-learn";
+const ROOTURL = `https://docs.astro.build/zh-cn/`;
+const rootURL = `https://docs.astro.build/zh-cn/getting-started/`;
 let visitedLinks = new Set();
 let pdfDocs = [];
 
@@ -51,7 +49,7 @@ const queue = async.queue(async function (task, callback) {
   await scrapePage(url, index);
 
   callback();
-}, 1); // Limit the concurrency to 15.
+}, 15); // Limit the concurrency to 15.
 
 queue.drain(async function () {
   console.log("All items have been processed");
@@ -105,22 +103,26 @@ async function scrapePage(url, index) {
   // set user agent to prevent the website from blocking our request
   await page.setUserAgent(userAgent);
   await page.goto(url, { waitUntil: "networkidle0" });
-  // const articleSelector = "main";
-  // await page.waitForSelector(articleSelector);
 
+  console.log(`Start scrolling ${url}...`)
   await autoScroll(page);
+  console.log(`End scrolling ${url}...`)
 
   // Here we are using the `evaluate` method to modify the page's DOM.
   await page.evaluate(() => {
     // Select all the content outside the <article> tags and remove it.
-    document.body.innerHTML = document.querySelector("main").outerHTML;
+    console.log("get article content...")
+    document.body.innerHTML = document.querySelector("article").outerHTML;
   });
 
-  const pdfPath = `${pdfDir}/${url.split("/").pop()}.pdf`;
+  const pdfName = url.split("/").filter(Boolean).pop();
+  console.log(`Start saving ${pdfName}...`)
+
+  const pdfPath = `${pdfDir}/${pdfName}.pdf`;
   await page.pdf({
     path: pdfPath,
-    format: "A4",
-    margin: { top: "1cm", right: "1cm", bottom: "1cm", left: "1cm" },
+    format: "A5",
+    margin: { top: "2cm", right: "1cm", bottom: "2cm", left: "1cm" },
   });
   pdfDocs.push({ pdfPath, index });
 
@@ -140,13 +142,11 @@ async function scrapeNavLinks(url) {
     await page.waitForSelector("aside");
   
     const navLinks = await getNavLinks(page);
-    // console.log(navLinks);
     let index = 0;
     const _rootURL = ROOTURL;
     console.log("_rootURL", _rootURL)
     for (let link of navLinks) {
       const fullLink = new URL(link, _rootURL).href;
-      // console.log("fullLink", fullLink)
       if (fullLink.startsWith(_rootURL)) {
         queue.push({ url: fullLink, index: index++ });
       }
@@ -161,21 +161,19 @@ async function scrapeNavLinks(url) {
 
 async function getNavLinks(page) {
   const navlinks = await page.evaluate(async () => {
-    const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
-
-    const button = document.querySelector('button.api-switch');
-    button.click();
-    await delay(1000); // Adjust the delay as needed to wait for sublinks to appear
-
     const links = Array.from(document.querySelectorAll('aside a'));
-    const navlinks = [];
+    let navlinks = [];
     for (const link of links) {
       navlinks.push(link.href);
     }
 
+    const startLink = 'https://docs.astro.build/zh-cn/getting-started/';
+    // filter navlinks that only start with ROOTURL
+    navlinks = navlinks.filter(link => link.startsWith(startLink) && link === startLink || !link.startsWith(startLink));
     return navlinks;
   });
-
+  console.log("navlinks", navlinks);
+  
   return navlinks;
 }
 
