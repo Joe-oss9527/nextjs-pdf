a24214e6f739de8e1943289c4a7f945e75ab2829
yvan yang <zjlgdx@gmail.com>
2024-03-09 21:00:47 +0800

    fix img load

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
   rootURL: "https://nextjs.org/docs",
   pdfDir: "./pdfs",
-  concurrency: 5,
+  concurrency: 10,
   navLinksSelector: "main nav.styled-scrollbar a[href]:not([href='#'])",
   contentSelector: "article",
   ignoreURLs: ["docs/pages", "docs/app/api-reference"]
